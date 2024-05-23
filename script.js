2c349f55fe683cdd528b5ef749c344a5ff129fc2
hassan memon <bhaihassan036@gmail.com>
2024-05-23 10:31:18 +0100

    javascript add

---
+++ script.js
@@ -0,0 +1,22 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navBar = document.querySelector(".navbar");
+
+
+let sections = document.querySelector("section");
+let navLinks = document.querySelector("header nav ul li a");
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+      links.classList.remove("active");
+      document.querySelector("header nav ul li a[href*=" + id + " ]" ).classList.add(active)
+            })
+        }
+    })
+}

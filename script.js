67811de162e49d3dce2cb17c092b93847fa22747
hassan memon <bhaihassan036@gmail.com>
2024-07-13 03:15:21 +0500

    smash

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.onscroll = () => {
         if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
       links.classList.remove("active");
-      document.querySelector("header nav ul li a[href*=" + id + " ]" ).classList.add(active)
+      document.querySelector("header nav ul li a[href*=" + id + " ]" ).classList.add("active");
             })
         }
     })
